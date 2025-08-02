efff1c8e786d3d852ed7bdb3523702b0363add78
Connor <your_email@example.com>
2025-08-02 00:25:58 -0700

    Initial Commit

---
+++ EQLE/EQLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  EQLEApp.swift
+//  EQLE
+//
+//  Created by Connor Podea on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct EQLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EQLETests/EQLETests.swift
@@ -0,0 +1,17 @@
+//
+//  EQLETests.swift
+//  EQLETests
+//
+//  Created by Connor Podea on 8/2/25.
+//
+
+import Testing
+@testable import EQLE
+
+struct EQLETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
